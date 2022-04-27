2e370f43dc20b2d6f93b72d43a0938d225cfe44a
Jay Hill <jayhill@utah.gov>
2022-04-27 14:22:01 -0600

    ui step closer

--- src/index.js
+++ src/index.js
@@ -4,22 +4,91 @@ import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import LabelClass from "@arcgis/core/layers/support/LabelClass";
+import LayerList from "@arcgis/core/widgets/LayerList";
+import Legend from "@arcgis/core/widgets/Legend";
+import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import { setAssetPath } from "@esri/calcite-components/dist/components";
 
 setAssetPath(location.href);
 
 
-const mainMap = new Map({
+const map = new Map({
   basemap: "streets-vector"
 });
 
 const view = new MapView({
   container: "viewDiv",
-  map: mainMap,
+  map: map,
   zoom: 7,
   center: [-111.68, 39.33]
 });
+
+view.when(() => {
+  let actionBarExpanded = false;
+  document.addEventListener("calciteActionBarToggle", (event) => {
+    actionBarExpanded = !actionBarExpanded;
+    view.padding = {
+      left: actionBarExpanded ? 135 : 45
+    };
+  });
+});
+
+
+document.querySelector("#header-title").textContent = "UGS Rockcore";
+
+
+let activeWidget;
+
+const handleActionBarClick = ({ target }) => {
+  console.log(target)
+  if (target.tagName !== "CALCITE-ACTION") {
+    return;
+  }
+
+  if (activeWidget) {
+    document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+    document.querySelector(`[data-panel-id=${activeWidget}]`).hidden = true;
+  }
+
+  const nextWidget = target.dataset.actionId;
+  if (nextWidget !== activeWidget) {
+    document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+    document.querySelector(`[data-panel-id=${nextWidget}]`).hidden = false;
+    activeWidget = nextWidget;
+  } else {
+    activeWidget = null;
+  }
+};
+
+document.querySelector("calcite-action-bar").addEventListener("click", handleActionBarClick);
+
+document.querySelector("calcite-shell").hidden = false;
+document.querySelector("calcite-loader").active = false;
+
+view.ui.move("zoom", "top-right");
+
+      const basemaps = new BasemapGallery({
+        view,
+        container: "basemaps-container"
+      });
+
+      const layerList = new LayerList({
+        view,
+        selectionEnabled: true,
+        container: "layers-container"
+      });
+
+      const legend = new Legend({
+        view,
+        container: "legend-container"
+      });
+
+
+
+
+
+
 
 var wellsUrl = "https://services.arcgis.com/ZzrwjTRez6FJiOq4/arcgis/rest/services/UCRC_Database_App_View/FeatureServer/0";
 
@@ -93,12 +162,12 @@ var townrange = new FeatureLayer({
   labelingInfo: [labelClass]
 });
 
-mainMap.add(rockcore);
-mainMap.add(rockcore)
-mainMap.add(fields);
-mainMap.add(townrange);
-mainMap.add(counties);
-mainMap.add(basins);
+map.add(rockcore);
+map.add(rockcore)
+map.add(fields);
+map.add(townrange);
+map.add(counties);
+map.add(basins);
 
 
 
